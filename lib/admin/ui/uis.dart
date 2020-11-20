e4d343fb890848249c8334e3d8f01ea17c347d6f
unknown <muhammadkhafidfuadi@gmail.com>
2020-11-20 13:52:24 +0700

    first commit

---
+++ lib/admin/ui/uis.dart
@@ -0,0 +1,11 @@
+import 'package:project_coba/admin/viewModel/viewModel.dart';
+import '../../size_helper.dart';
+import 'package:project_coba/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+part 'absenHarian.dart';
+part 'admin.dart';
+part 'homeAdmin.dart';
+part 'dataSingleAbsen.dart';
+part 'tambahKaryawan.dart';
+// part 'ubahLokasi.dart';
